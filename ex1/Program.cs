f0389460ac6c6f9bf50bf5f5e9411968754a9e15
yasya <kalgexavva1970@icloud.com>
2022-11-07 21:14:08 +0300

    add new text

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//напишите программу,котрая принимает число А и выдает количество чисел
+
+int GetSum (int A)
+{
+    int sum=0;
+
+    while (A!=0)
+    {A=A/10;//достаем каждое число (10-система счисления)
+    sum++;
+    }
+    return sum;
+}
+Console.Write("введите число:");
+int N=int.Parse(Console.ReadLine()!);//считываеь и образует строку
+Console.WriteLine(GetSum(N));
+
+
