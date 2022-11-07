3540b1223e36b9395aa108c0a39077719a5158a2
yasya <kalgexavva1970@icloud.com>
2022-11-07 23:08:37 +0300

    add new file

---
+++ DZ2/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetSum (int A)
+{
+    int sum=0;
+    for(int i=1;i<=A;i++)
+    { sum+=i;
+
+    }
+    
+    return sum;
+}
+Console.Write("введите число:");
+int N=int.Parse(Console.ReadLine()!);//считываеь и образует строку
+Console.WriteLine(GetSum(N));
