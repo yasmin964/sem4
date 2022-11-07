a8b6d7f3425fc6dd634ea117bcd8b06bd3230c19
yasya <kalgexavva1970@icloud.com>
2022-11-07 23:32:39 +0300

    add new text

---
+++ dz3/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу,которая задает масси из 8 элементов и выводит их на экран
+int [] GetRandomArray()
+{
+    int []arr=new int [8];
+    for (int i=0;i<8;i++);
+    {
+        arr[i]=Next(0,2);
+    }
+    return arr;}
+Console.Write("Введите 8 чисел:");
+int N=int.Parse(Console.Readline()!);
+Console.WriteLine(GetRandomArray(N));
