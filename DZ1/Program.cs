1b4a73391f31a1facabdf2909c53c4137a27cef2
yasya <kalgexavva1970@icloud.com>
2022-11-07 23:13:04 +0300

    add new file

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,15 +1,20 @@
 ﻿//напишите цикл который принимает на вход два числа a и b и возводит число a в натуральную степень b
 int GetPow(int A,int B)
-{
+{{
     int ans=1;
     for (int i=1;i<=B;i++);
     {
         ans*=A;
     }
-    return ans;
-}
+    return ans;}
+    return B;}
+
+
+
 Console.Write("введите число а:");
 Console.Write("введите число b:");
 int N=int.Parse(Console.ReadLine()!);
-Console.WriteLine(GetPow(N));
+int c=int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetPow(N,c));
+
 
