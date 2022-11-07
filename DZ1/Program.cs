cfca91c6d5adcffb3a1d9b1c8d5455103821ab6f
yasya <kalgexavva1970@icloud.com>
2022-11-07 22:29:46 +0300

    add new text

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//напишите цикл который принимает на вход два числа a и b и возводит число a в натуральную степень b
+int GetPow(int A,int B)
+{
+    int ans=1;
+    for (int i=1;i<=B;i++);
+    {
+        ans*=A;
+    }
+    return ans;
+}
+int numbera = ReadInt("Введите число A: ");
+int numberb = ReadInt("Введите число B: ");
+
+
