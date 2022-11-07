5244c7e05d9067db67d0f007ab0fb69f325abf42
yasya <kalgexavva1970@icloud.com>
2022-11-07 21:49:18 +0300

    add new file

---
+++ ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿//написать программу котрая выводит масссив из 8 элементов состоящи1 из 1,0 в случайной последовательности
+int [] GetRandomArray()
+{
+    int []arr=new int [8];
+    for (int i=0;i<8;i++);
+    {
+        arr[i]=new Random().Next(0,2);
+    }
+    return arr;
+}
+console.write("[");
+for (int i =0;i<7;i++);
+console.write($"{result[i]}");
+Console.WriteLine();
+Console.writeline();
