06c9e6aeb16cd96699e8a82a1d6c217205bd2fcd
yasya <kalgexavva1970@icloud.com>
2022-11-07 22:01:41 +0300

    add new text

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,15 +1,17 @@
 ﻿//напишите программу,котрая принимает число А и выдает произвеедение чисел,входящих в него
-int GetСol (int A)
-{
-    int sum=1;
+// int GetProductofnumbers (int A)
+// {
+//     int sum=1;
 
-    for(int i =1;i<=A;i++)
-    {
-    sum=sum*i;
-    }
-    return sum;
-}
-Console.Write("введите число:");
-int N=int.Parse(Console.ReadLine()!);//считываеь и образует строку
-Console.WriteLine(GetCol(N));
+//     for(int i =1;i<=A;i++)
+//     {
+//     sum*=i;
+//     }
+//     return sum;
+// }
+// Console.Write("введите число:");
+// int N=int.Parse(Console.ReadLine()!);//считывает и образует строку
+// Console.WriteLine(GetProductofnumbers(N));
+
+
 
